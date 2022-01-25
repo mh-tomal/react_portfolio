d92cacab0dfef49606320e206af3ed1de5807539
Mahmudul Hasan <mahmudulhasan2020@gmail.com>
2022-01-25 22:06:13 +0600

    project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,24 @@
 export default function Projects() {
   return (
-    <div>
-      <h1>projects</h1>
+    <div className="mt-20">
+      <div className="h-screen">
+        <div className="h-3/4">
+          <lottie-player
+            src="https://assets2.lottiefiles.com/packages/lf20_ygiuluqn.json"
+            background="transparent"
+            speed="1"
+            loop
+            autoplay
+          ></lottie-player>
+        </div>
+        <p className="text-xl font-semibold text-center">
+          Good ideas are not adopted automatically. They must be driven into
+          practice with courageous patience
+        </p>
+        <h1 className="text-4xl text-center font-bold mt-5" data-aos="slide-up">
+          Because
+        </h1>
+      </div>
     </div>
   );
 }
